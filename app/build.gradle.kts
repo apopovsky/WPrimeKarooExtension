5fc98506998ba6d4a5d66cdcf05a370f9fe72757
Ariel Popovsky <apopovsky@gmail.com>
2026-05-22 20:02:30 -0300

    Bump extension version to 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ configure<com.android.build.api.dsl.ApplicationExtension> {
         applicationId = "com.itl.wprimeext"
         minSdk = 23
         targetSdk = 37
-        versionCode = 10
-        versionName = "1.1.0"
+        versionCode = 11
+        versionName = "1.1.1"
         base.archivesName.set("WPrimeExtension-v${versionName}")
     }
 
