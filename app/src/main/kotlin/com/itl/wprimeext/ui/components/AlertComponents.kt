633e231fa0c569aa2e1b3ab3f404ea0bbeb9e5fa
Ariel Popovsky <ariel.popovsky@un.org>
2026-06-02 09:06:25 -0300

    chore: update ksp version to 2.3.8 and remove unused import in AlertComponents

--- app/src/main/kotlin/com/itl/wprimeext/ui/components/AlertComponents.kt
+++ app/src/main/kotlin/com/itl/wprimeext/ui/components/AlertComponents.kt
@@ -37,7 +37,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.DialogProperties
 import com.itl.wprimeext.extension.AlertType
 import com.itl.wprimeext.extension.WPrimeAlert
 
